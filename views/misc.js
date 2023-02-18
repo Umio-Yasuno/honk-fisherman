beac709d32f914fae3e58fc4643ae02d2f9805da
Umio Yasuno <53935716+Umio-Yasuno@users.noreply.github.com>
2023-02-19 08:24:11 +0900

    update views/chatter

--- views/misc.js
+++ views/misc.js
@@ -2,4 +2,7 @@
   document.querySelectorAll(`.info.emus .emu`).forEach((el) => {
     el.onclick = async () => { await navigator.clipboard.writeText(el.alt) };
   });
+  document.querySelectorAll(`label.donker`).forEach((el) => {
+    el.onchange = () => { el.dataset.filename = `...` + el.children[0].value.slice(-20) };
+  });
 })();
