014a06475ee5977f07d9a7f32aeff1fe11ca255a
Umio Yasuno <53935716+Umio-Yasuno@users.noreply.github.com>
2023-02-25 11:43:25 +0900

    replace inline img with a tag link

--- fun.go
+++ fun.go
@@ -204,7 +204,7 @@ func replaceimgsand(zap map[string]bool, absolute bool) func(node *html.Node) st
 			}
 			return string(templates.Sprintf(`<img alt="%s" title="%s" src="%s/d/%s" loading="lazy">`, alt, alt, base, d.XID))
 		}
-		return string(templates.Sprintf(`&lt;img alt="%s" src="<a href="%s">%s</a>"&gt;`, alt, src, src))
+		return string(templates.Sprintf(`<a href="%s">%s</a>`, src, src))
 	}
 }
 
